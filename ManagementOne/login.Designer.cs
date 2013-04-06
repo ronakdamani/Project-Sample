b615345f466de9ef22c0e88ddb01c2668dd840cc
Ronak Damani <ronakdamani0@gmail.com>
2013-04-06 15:50:19 +0530

    added login form

--- ManagementOne/login.Designer.cs
+++ ManagementOne/login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ManagementOne
 {
-    partial class Form1
+    partial class login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 264);
-            this.IsMdiContainer = true;
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            this.Text = "login";
         }
 
         #endregion
     }
 }
-
